0f25905ec72d149664d5903cc249ecc80c10ec42
Lance Batson <lancebatsondev@gmail.com>
2019-12-06 09:41:57 -0600

    add git alias commands

--- zsh/custom/git.zsh
+++ zsh/custom/git.zsh
@@ -5,3 +5,6 @@ alias glog="git log --color --graph\
     --abbrev-commit"
 alias gru='git remote update --prune'
 alias gtags="git tag -l -n1"
+alias gignore="!git update-index --assume-unchanged"
+alias gunignore="!git update-index --no-assume-unchanged"
+alias gignored="!git ls-files -v | grep ^[a-z]"
